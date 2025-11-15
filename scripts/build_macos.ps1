4f47cec7ed5a5e1783f9d8d77a745b5144d602fe
JonasPfalzgraf <20913954+JosunLP@users.noreply.github.com>
2025-11-15 01:16:35 +0100

    Set version to 1.0.0 in all relevant files for the release

--- scripts/build_macos.ps1
+++ scripts/build_macos.ps1
@@ -16,7 +16,7 @@ $ErrorActionPreference = "Stop"
 # Konfiguration
 $NAME = "HypnoScript"
 $BUNDLE_ID = "com.kinkdev.hypnoscript"
-$VERSION = "1.0.0-rc4"
+$VERSION = "1.0.0"
 $BINARY_NAME = "hypnoscript-cli"
 $INSTALL_NAME = "hypnoscript"
 
